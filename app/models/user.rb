54265e6cc633377816019e7b4dc9298e69077c6d
xwzsd <shamik2024@gmail.com>
2018-08-17 21:44:18 +0300

    add_some_models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,17 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :jwt_authenticatable,
+         jwt_revocation_strategy: JwtBlacklist
+
+  has_one :user_profile, dependent: :destroy
+
+  before_create :callback_before_create
+
+  validates :email, :password, presence: true
+  validates :email, format: { with: /\A[^@\s]+@([^@.\s]+\.)+[^@.\s]+\z/ }
+
+  def callback_before_create
+    build_user_profile
+    true
+  end
 end
